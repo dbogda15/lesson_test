831c285c4a50a90bc1337b129d6f873d4fc25ebc
dianabogdanova <dbogda15@yandex.ru>
2023-04-17 22:06:28 +0500

    initial commit

---
+++ src/main/java/me/dbogda/lesson_test/LessonTestApplication.java
@@ -0,0 +1,13 @@
+package me.dbogda.lesson_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LessonTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LessonTestApplication.class, args);
+    }
+
+}
